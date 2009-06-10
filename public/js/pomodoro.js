c79748e37622bebdb53699832a25cd109e0cd670
Stephen Caudill <vox@exdolo.com>
2009-06-10 09:31:43 +0800

    Keyboard shortcuts for timers (opt+s,opt+l,opt+p)
    Signed-off-by: sandro turriate <sandro.turriate@gmail.com>

--- public/js/pomodoro.js
+++ public/js/pomodoro.js
@@ -9,20 +9,15 @@ $.countdown.setDefaults({alwaysExpire: true, format: 'MS'})
 soundManager.url = 'swf/'
 
 var isCtrl = false;
-var ctrlKey = 17;
-var pKey = 80;
-var sKey = 83;
-var lKey = 76;
-
 $(document).keyup(function(e){
-  if(e.which == ctrlKey) isCtrl = false;
+  if(e.which == 18) isOpt = false;
 });
 
 $(document).keydown(function(e){
   // console.log(e.type,"keyCode:"+e.keyCode,"which:"+e.which,"charCode:"+String.fromCharCode(e.which));
-  if(e.which == ctrlKey) isCtrl = true;
-  if(isCtrl){
-    if($.inArray(e.keyCode, [pKey,sKey,lKey]) > -1){
+  if(e.which == 18) isOpt = true;
+  if(isOpt){
+    if($.inArray(e.keyCode, [80,83,76]) > -1){
       $('#' + String.fromCharCode(e.which).toLowerCase()).submit();
     }
   }
